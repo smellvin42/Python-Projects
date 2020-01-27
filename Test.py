be3ac7d5e9ac6aab1f31f958de855047d0a5bcdc
smellvin42 <57145251+smellvin42@users.noreply.github.com>
2020-01-27 10:34:54 -0600

    Create Test.py
    completed assignment

---
+++ Test.py
@@ -0,0 +1,39 @@
+a=3
+a**2 >= 9 and not a>3
+a+2 ==5 or a-1 !=3
+x, y = (325,325)
+
+def age_limit_output(age):
+    age_limit = 13
+    if age <age_limit:
+        print(age, 'is below age limit')
+    else:
+        print(age, 'is old enough')
+    print('minimum age is', age_limit)
+
+age_limit_output(15)
+
+def report_grade(percent):
+    if percent >=80:
+        "a grade of %s"
+
+report_grade(79)
+#a grade of 79 does not indicate mastery
+#Seek extra practice or help
+
+def vowel(Letter):
+    vowels="AEIOUaeiou"
+if Letter in vowels:
+    return True
+else:
+    return False
+
+vowel(a)
+
+def letter_in_word(guess, word):
+  guess="howdyallHOWDYALL"
+  if : letter in letters
+      return True
+  else:
+      return False
+
