4b8ce743fb7a60d1371cc4cf44480fa69adea973
smellvin42 <57145251+smellvin42@users.noreply.github.com>
2020-01-31 10:44:43 -0600

    it work now

--- Test.py
+++ Test.py
@@ -1,9 +1,11 @@
-a=3
+
+ a=3
+
 a**2 >= 9 and not a>3
 # prediction: false
 
 a+2 ==5 or a-1 !=3
-#Prediction: True
+#'Prediction: True'
 x, y = (325,325)
 
 def age_limit_output(age):
@@ -14,11 +16,13 @@ def age_limit_output(age):
         print(age, 'is old enough')
     print('minimum age is', age_limit)
 
+
 age_limit_output(15)
 
-def report_grade(percent):
+
+ def report_grade(percent):
     if percent >=80:
-        "a grade of %s"
+        print("a grade of %s")
 
 report_grade(79)
 #a grade of 79 does not indicate mastery
@@ -26,17 +30,52 @@ report_grade(79)
 
 def vowel(letter):
     vowels="AEIOUaeiou"
-if letter in vowels:
-    print(True)
-else:
-    print(False)
+    if letter in vowels:
+        return True
+    else:
+        print(False)
+
 
 vowel(a)
 
+
+
 def letter_in_word(letter):
   letter_in_word="howdyallHOWDYALL"
-  if  letter in letter_in_word
-    print(true)
-  else : return False print(false)
+  if  letter in letter_in_word:
+    return True
+  else:
+      return False
 
-letter_in_word(r)
+letter_in_word(R)
+
+def intiger(n):
+    if int(n)==n:
+        if n % 2 == 0:
+            if n % 3 ==0:
+                print('number is a multiple of 6' )
+            else:
+                print('number is even')
+        else:
+            print( 'number is odd')
+    else:
+        print('number is not an integer')
+
+
+intiger(12)
+
+
+
+import random
+def guess_once():
+    secret = random.randint(1, 4)
+    print('I have a number between 1 and 4.')
+    guess = int(raw_input('Guess: '))
+    if guess != secret:
+        print('Wrong, my number is ', secret, '.', sep='')
+    else:
+        print('Right, my number is', guess, end='!\n')
+
+
+
+
