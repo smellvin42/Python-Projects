813ad19e78c1a04891b387da11a3c6ae1a4f5c6b
smellvin42 <57145251+smellvin42@users.noreply.github.com>
2020-02-10 10:41:01 -0600

    it work now

--- 3.3.5 Python.py
+++ 3.3.5 Python.py
@@ -3,7 +3,7 @@ if input('One Character:') == '!':
 
 print('------------------------------------------------------------------')
 
-slogan = 'My school sucks'
+slogan = 'My school is the best'
 type(slogan)
 
 print(slogan)
@@ -22,3 +22,21 @@ print(slogan[8])
 print(slogan[1])
 
 print('------------------------------------------------------------------')
+
+print(slogan[0:12])
+
+print('------------------------------------------------------------------')
+
+print(slogan[:5])
+print(slogan[17:21])
+print(slogan[:13] + 'Awesome!')
+
+print('------------------------------------------------------------------')
+
+print(len(slogan))
+activity = 'Theater'
+print(len(activity))
+print(activity[0: len(activity)-1])
+
+print('------------------------------------------------------------------')
+def how_eligible(essay): v
